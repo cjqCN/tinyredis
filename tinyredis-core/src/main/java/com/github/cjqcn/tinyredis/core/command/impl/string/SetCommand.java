bea3400dbbe3a714b0eaa8fb291d9c7898c594dc
chenjinquan <15521255460@163.com>
2020-02-29 21:19:00 +0800

    fix

--- tinyredis-core/src/main/java/com/github/cjqcn/tinyredis/core/command/impl/string/SetCommand.java
+++ tinyredis-core/src/main/java/com/github/cjqcn/tinyredis/core/command/impl/string/SetCommand.java
@@ -38,7 +38,7 @@ public class SetCommand extends AbstractCommand implements RedisCommand {
     @Override
     public String decode() {
         if (expireSec != null) {
-            return String.format("set %s %s ex %d", key, value, expireSec);
+            return String.format("set %s ex %s %d", key, value, expireSec);
         }
         return String.format("set %s %s", key, value);
     }
